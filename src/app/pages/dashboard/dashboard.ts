dc2e291e755e0c2ae6639a470a97c051cbf99901
the-devweb <le-codeur@outlook.fr>
2026-05-14 15:50:48 +0200

    add admin dashboard and see pending validation of user in it add state of user subscription navbar menu close when user clic on a link user can see its password on login form improve visuals and display code cleaning

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -2,14 +2,15 @@ import { Component, computed, inject } from '@angular/core';
 import { AuthService } from '../../core/services/auth.service';
 import { ProfessionalDashboard } from './professional/professional-dashboard';
 import { IndividualDashboard } from './individual/individual-dashboard';
-// import { AdminDashboard } from './admin/admin-dashboard';
+import { AdminDashboard } from './admin/admin-dashboard';
 
 @Component({
   standalone: true,
-  imports: [ProfessionalDashboard, IndividualDashboard/* , AdminDashboard */],
+  imports: [ProfessionalDashboard, IndividualDashboard, AdminDashboard],
   templateUrl: './dashboard.html',
 })
 export class DashboardPage {
   private readonly authService = inject(AuthService);
+  readonly currentUser = computed(() => this.authService.currentUser());
   readonly role = computed(() => this.authService.currentUser()?.role?.code ?? null);
 }
